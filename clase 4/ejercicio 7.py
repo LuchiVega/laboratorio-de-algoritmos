0ebd785faecb7a8d0c6d0a0e6661f8fd842007f9
LuchiVega <luchivega1212@gmail.com>
2025-03-19 10:19:08 -0300

    octavo commit

---
+++ clase 4/ejercicio 7.py
@@ -0,0 +1,9 @@
+marca_de_auto = []
+
+marca_de_auto.append("Lamborgini")
+marca_de_auto.append("Ferrari")
+marca_de_auto.append("Porsche")
+marca_de_auto.append("Bugati")
+
+for auto in marca_de_auto:
+    print(auto)
